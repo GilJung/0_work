563fb95ecfd3430ba1e13de15c5d34ea8dc44908
Gil Jung <kipumjung@gmail.com>
2024-06-04 09:34:02 +0000

    feat: config for Orin  - config file generation for Orin

---
+++ config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 19 14:22:42 2018
+
+@author: christophers
+"""
+
+processor_register = {
+        "T234" : "current_dram_clktree_c15d1b1",
+        "T186" : "current_dram_clk_tree_c3d1u1"            
+}
